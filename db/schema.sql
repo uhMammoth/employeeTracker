61bd37b1914fdd597fdaba910a21ddff94e2f580
KyleP <kyl3p3tty@gmail.com>
2022-05-15 20:52:30 -0700

    removed routes folder as express is not needed added prompts for viewing tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS employees
-DROP TABLE IF EXISTS roles
-DROP TABLE IF EXISTS departments
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,27 +1,105 @@
-const express = require('express');
 const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-//middleware for express
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-
-//using api routes
-app.use('/api', apiRoutes);
-
-//default response
-app.use((req, res) => {
-    res.status(404).end();
-});
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 // starts server after database connection
 db.connect(err => {
     if(err) throw err;
     console.log('Database connected.');
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
 });
+
+
+// const addEmployee = [{}];
+
+function promptHandler(){
+    const choices = ['View All Departments','View All Roles','View All Employees','Add Department','Add Role','Add Employee','Update An Employees Role', 'Exit'];
+    inquirer.prompt([{
+        type: 'list',
+        name: 'choice',
+        message: 'What would you like to do?',
+        choices: choices
+    }]).then(answers => {
+        let sql = '';
+        switch (answers.choice) {
+            case 'View All Departments':
+                sql = `SELECT * FROM departments`; 
+                viewAll(sql);               
+                break;
+            case 'View All Roles':
+                sql = `SELECT * FROM roles`; 
+                viewAll(sql);
+                break;
+            case 'View All Employees':
+                sql = `SELECT * FROM employees`; 
+                viewAll(sql);
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Add Employee':
+        
+                break;
+            case 'Update An Employees Role':
+
+                break;
+            case 'Exit':
+                return;
+        }
+        
+    }); 
+};
+
+function addDepartment(){
+    const sql = `INSERT INTO departments (name) VALUES (?)`;
+    inquirer.prompt({
+        type: 'input',
+        name: 'departmentName',
+        message: 'Enter The New Department: '
+    })
+    .then(input => {
+        console.log(input.departmentName);
+        db.query(sql, input.departmentName, (err, rows) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+        });
+        promptHandler();
+    });    
+};
+
+function addRole(){
+    const addRole = [{
+        type: 'text',
+        name: 'roleName',
+        message: 'Enter The New Role: '
+    },{
+        type: 'text',
+        name: 'salary',
+        message: 'Enter Salary For This Role: '
+    },];
+    inquirer.prompt({
+        
+    })
+    .then()
+};
+
+function viewAll(sql){
+    
+    db.query(sql, (err, rows) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.info('\n=============');
+        console.table(rows);
+        console.info('=============');
+        promptHandler();
+    });
+    return;
+};
+
+promptHandler();
